35dda554fac66b596aff4db34e76df102a1a1b31
keni alexsander <keni4291989@gmail.com>
2024-04-05 09:22:39 +0530

    My First React App

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Greet />
+    </>
+  );
+}
+function Greet() {
+  return (
+    <div>
+      <h1>Hi Keni Welcome To React...!</h1>
+    </div>
+  );
+}
+
+export default App;
